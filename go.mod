24d5dbb31d39e798adaa83cf60196e8bb4c7d4eb
sanja_s <stovpyaga@gmail.com>
2022-08-03 21:30:48 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sanjas12/Go_snippetbox
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+
+func home(w http.ResponseWriter, r *http.Request)  {
+	w.Write([]byte("Hello from snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request)  {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request)  {
+	w.Write([]byte("Create a new snippet	"))
+}
+
+
+
+func main()  {
+	mux :=http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Starting server on :4000")
+	err :=http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
+
+
